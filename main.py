f58184f9f7c5221b344292812481938b56634722
Kobus <kobus.vs.2006@gmail.com>
2025-11-05 15:39:33 +0200

    Git init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vsi!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ smartmoney/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartmoneyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smartmoney'
